ade172df65f2651935bc056edf4bad218523e3af
QiangYin1991 <qiangyin1991@gmail.com>
2018-12-31 12:26:38 +0800

    先提交，正则表达式有问题

--- chapter5/5-5/index.js
+++ chapter5/5-5/index.js
@@ -29,10 +29,19 @@ const app = new Vue({
                 let item = this.list[i];
                 total += item.price * item.count;
             }
-            return total.toString().replace(/\B(?=(\d{3}+$))/g, ',);
+            return total.toString().replace(/\B(?=(\d{3}+$))/g, ',');
         }
     },
     methods: {
-
+        handleReduce: function (index) {
+            if (this.list[index].count === 1) return;
+            this.list[index].count--;
+        },
+        handleAdd: function (index) {
+            this.list[index].count++;
+        },
+        handleRemove: function (index) {
+            this.list.splice(index, 1);
+        }
     }
 });
