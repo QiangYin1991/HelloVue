235ca7d7804ee16666a27cbed8f7cb0727861cbe
SOHU-INC\qiangyin <qiangyin@sohu-inc.com>
2019-03-07 15:41:37 +0800

    vue 组件化

--- chapter10/app.vue
+++ chapter10/app.vue
@@ -1,13 +1,21 @@
 <template>
   <div>
-    Hello {{ name }}   
+    <v-title title="Vue 组件化"></v-title>
+    <v-button @click="handleClick">点击按钮</v-button> 
   </div>
 </template>
 <script>
+import vTitle from './title.vue';
+import vButton from './button.vue';
+
 export default {
-  data () {
-    return {
-      name: 'Vue.js'
+  components: {
+    vTitle,
+    vButton
+  },
+  methods: {
+    handleClick (e) {
+      console.log(e);
     }
   }
 }
