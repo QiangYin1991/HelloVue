c480e064a2f9f8e8bb7b8e599309598feb66e121
QiangYin1991 <qiangyin1991@gmail.com>
2019-03-12 23:58:19 +0800

    数字输入框

--- chapter7/7-7-1/input-number.js
+++ chapter7/7-7-1/input-number.js
@@ -1,6 +1,9 @@
 Vue.component('inputNumber', {
   template: '\
   <div class="input-number"> \
+    <input type="text" :value="currentValue" @change="handleChange"></input> \
+    <button @click="handleDown" :disabled="currentValue <= min">-</button> \
+    <button @click="handleUp" :disabled="currentValue >= max">+</button> \
   </div>',
   props: {
     max: {
@@ -21,4 +24,51 @@ Vue.component('inputNumber', {
       currentValue: this.value
     }
   },
+  watch: {
+    currentValue: function (val) {
+      this.$emit('input', val);
+      this.$emit('on-change', val);
+    },
+    value: function(val) {
+      this.updateValue(val);
+    }
+  },
+  methods: {
+    updateValue (val) {
+      if (val > this.max) val = this.max;
+      if (val < this.min) val = this.min;
+      this.currentValue = val;
+    },
+    handleDown () {
+      if (this.currentValue <= this.min) return;
+      this.currentValue -= 1;
+    },
+    handleUp () {
+      if (this.currentValue >= this.max) return;
+      this.currentValue += 1;
+    },
+    handleChange (event) {
+      let val = event.target.value.trim();
+      console.log(val);
+      if (isValueNumber(val)) {
+        val = Number(val);
+        this.currentValue = val;
+
+        if (val > this.max) {
+          this.currentValue = this.max;
+        } else if (val < this.min) {
+          this.currentValue = this.min;
+        }
+      } else {
+        event.target.value = this.currentValue;
+      }
+    }
+  },
+  mounted: function() {
+    this.updateValue(this.value);
+  }
 });
+
+function isValueNumber (value) {
+  return (/(^-?[0-9]+\.{1}\d+$)|(^-?[1-9][0-9]*$)|(^-?0{1}$)/). test(value + '');
+}
