5d1919f953dea4941df003b591a660cb5ac9cdf6
QiangYin1991 <qiangyin1991@gmail.com>
2019-04-02 23:28:09 +0800

    提交部分

--- chapter13/daily/app.vue
+++ chapter13/daily/app.vue
@@ -1,8 +1,16 @@
 <template>
    <div class="daily">
        <div class="daily-menu">
-           <div class="daily-menu-item">每日推荐</div>
-           <div class="daily-menu-item">主题日报</div>
+           <div class="daily-menu-item"
+                :class="{on: type === 'recommend'}">每日推荐</div>
+           <div class="daily-menu-item"
+                :class="{on: type === 'daily'}"
+                @click="showThemes = !showThemes">主题日报</div>
+           <ul v-show="showThemes">
+               <li v-for="item in themes">
+                   <a :class="{ on: item.id === themeId && type === 'daily'}">{{ item.name }}</a>
+               </li>
+           </ul>
        </div>
        <div class="daily-list">
            <Item></Item>
@@ -10,3 +18,27 @@
        <daily-article></daily-article>
    </div> 
 </template>
+
+<script>
+    import $ from './libs/util';
+    export default {
+        data () {
+            return {
+                themes: [],
+                showThemes: false,
+                type: 'recommend',
+                themeId: 0
+            }
+        },
+        methods: {
+            getThemes () {
+                $.ajax.get('themes').then(res => {
+                    this.themes = res.others;
+                });
+            }
+        },
+        mounted () {
+            this.getThemes();
+        }
+    }
+</script>
