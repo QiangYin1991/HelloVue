fa8d3e85d27511a76f8e7cf3b1abe0febaa75df1
QiangYin1991 <qiangyin1991@gmail.com>
2019-11-28 23:14:38 +0800

    vue-router

---
+++ Vue-cli/vuecliTest/src/components/Hi.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h2>{{msg}}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hi',
+  data: function () {
+    return {
+      msg: 'HiHiHi~~~'
+    }
+  }
+}
+</script>
+
+<style>
+</style>
